f03bedf9e92ea4b545a399c163b984ec7612eed0
sd <sd_net@mail.ru>
2023-10-28 20:24:40 +0300

    feat: added getDigitalRoot

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -279,8 +279,11 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  if (num < 10) return num;
+  return getDigitalRoot(num.toString().split('').reduce(
+    (acc, el) => acc + Number(el), 0,
+  ));
 }
 
 /**
